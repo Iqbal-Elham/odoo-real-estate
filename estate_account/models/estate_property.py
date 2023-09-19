00cb661551076e7a69060e9557ea182106a4da5d
iqbal-elham <iqbal.elham7@gmail.com>
2023-09-19 09:09:27 +0430

    Add the open_property method from invoice

--- estate_account/models/estate_property.py
+++ estate_account/models/estate_property.py
@@ -13,6 +13,7 @@ class EstateProperty(models.Model):
         invoice = self.env['account.move'].create({
             'partner_id': self.buyer.id,
             'move_type': 'out_invoice',
+            'property_id': self.id,
 
             'invoice_line_ids': [
                 Command.create({
@@ -41,4 +42,19 @@ class EstateProperty(models.Model):
                 'name': 'Invoice',
                 'res_model': 'account.move',
                 'res_id': rec.invoice_id.id,
+            }
+        
+class MoveAccount(models.Model):
+    _inherit = 'account.move'
+
+    property_id = fields.Many2one('estate.property', string='Property')
+
+    def open_property(self):
+        for rec in self:
+            return {
+                'type': 'ir.actions.act_window',
+                'view_mode': 'form',
+                'name': 'Property',
+                'res_model': 'estate.property',
+                'res_id': rec.property_id.id,
             }
