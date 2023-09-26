81b59ceffaf6de042323c897adaa378405ddc079
iqbal-elham <iqbal.elham7@gmail.com>
2023-09-26 13:13:46 +0430

    Add the views in manifest

--- banking_system/__manifest__.py
+++ banking_system/__manifest__.py
@@ -2,16 +2,18 @@
 {
     'name': "banking_system",
 
-    'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+    'summary': "Here you can use banking system's deposit management system, "
+               "like multiple banks handle, customers profile and its balance, "
+               "transaction details and notification you can send also print "
+               "transaction receipt.",
 
-    'description': """
-        Long description of module's purpose
-    """,
+    'description': "Here you can use banking system's deposit management system, "
+               "like multiple banks handle, customers profile and its balance, "
+               "transaction details and notification you can send also print "
+               "transaction receipt.",
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Iqbal Elham",
+    'website': "https://iqbal-elham.onrender.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/16.0/odoo/addons/base/data/ir_module_category_data.xml
@@ -19,14 +21,18 @@
     'category': 'Uncategorized',
     'version': '0.1',
 
+    'application': True,
+    'sequence': -1001,
+
     # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base', 'contacts', 'mail'],
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+        'security/ir.model.access.csv',
+        'views/bank_view.xml',
+        'views/banker_customer_view.xml',
+        'views/banker_transactions.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
